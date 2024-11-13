f7608147bc56bcecbf0215e17cdc9e112efa1215
junsoo0719 <junsoo0719@gmail.com>
2024-11-13 14:19:02 +0900

    문해기10주차

---
+++ 10_A.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int t;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> t;
+	while (t--)
+	{
+		int n, inha;
+		cin >> n;
+		cin >> inha;
+		priority_queue<int> pq; // O(N) space
+		for (int i = 0; i < n - 1; i++)
+		{
+			int sticker;
+			cin >> sticker;
+			if (sticker >= inha) // 추가
+				pq.push(sticker); // O(N log N) time
+		}
+		int max_sticker;
+		if (!pq.empty())
+			max_sticker = pq.top();
+		else
+			max_sticker = 0;
+		int move_cnt = 0;
+		while (inha <= max_sticker)
+		{
+			pq.pop();
+			inha++;
+			move_cnt++;
+			pq.push(--max_sticker); // O(log N) time
+			max_sticker = pq.top();
+		} // O(k log N) time
+		cout << move_cnt << "\n";
+
+		/*int max_sticker = 0;
+		while (!pq.empty() && inha <= pq.top())
+		{
+			max_sticker = pq.top();
+			pq.pop();
+			pq.push(max_sticker - 1);
+			inha++;
+			move_cnt++;
+		}*/
+	} // 전체 O((N + K)log N) time
+}
+
+/*
+ 정답률
+A 12/28
+B 0/28
+전체분반
+A 57/135
+B 10/135
+
+최악: 모든 학생 중 인하를 제외한 한 명이 모든 표를 다 가져간 경우
+-> 해당 학생의 보드판에서 스티커를 모두 떼어내어 인하의 보드판에
+붙여야 하므로, 스티커의 개수에 대해 k/2 + 1번 수행해야 함.
+
+ComputeCnt()
+int cnt = 0
+while !pq.empty() && inha <= pq.top()
+	int top = pq.top()
+	pq.pop()
+	pq.push(top-1)
+	inha++
+	cnt++
+return cnt
+*/
