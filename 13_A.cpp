3281f35c15b877f715c570ea7110c9b151d13e2a
junsoo0719 <junsoo0719@gmail.com>
2024-12-20 02:53:25 +0900

    문해기13주차 수정

--- 13_A.cpp
+++ 13_A.cpp
@@ -3,27 +3,21 @@
 #include <vector>
 using namespace std;
 
-int t;
-int A[1000][1000];
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
+	int t;
 	cin >> t;
 	while (t--)
 	{
 		int n, m;
 		cin >> n >> m;
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < m; j++)
-			{
-				cin >> A[i][j];
-			} //초기화 안 해도 덮어써서 괜찮음
-		}
+		vector<int> A(n * m); //O(NM) space n*m 1차원 배열
+		for (int i = 0; i < n * m; i++)
+			cin >> A[i];
 
 		int q;
 		cin >> q;
@@ -31,43 +25,12 @@ int main()
 		{
 			int query;
 			cin >> query;
-			bool found = false;
-			for (int j = 0; j < n; j++)
-			{
-				if (A[j][0] <= query && query <= A[j][m - 1])
-				{
-					if (binary_search(A[j], A[j] + m, query))
-					{
-						found = true;
-						break;
-					}
-				}
-			}
-			if(found)
+			if (binary_search(A.begin(), A.end(), query)) //존재 여부
 				cout << "1 ";
 			else
 				cout << "0 ";
 		}
 		cout << "\n";
-
-		/* 1차원 배열로도 가능
-		vector<int> A_flat(n * m); //O(NM) space
-		for (int i = 0; i < n * m; i++)
-		{
-			cin >> A_flat[i];
-		}
-
-		for (int i = 0; i < q; i++)
-		{
-			int query;
-			cin >> query;
-			if (binary_search(A_flat.begin(), A_flat.end(), query))
-				cout << "1 ";
-			else
-				cout << "0 ";
-		}
-		cout << "\n";
-		*/
 	} //O(Q log NM) time
 }
 
